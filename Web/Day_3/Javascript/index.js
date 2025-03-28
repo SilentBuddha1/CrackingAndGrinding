fafdd8c5389b0f97b5ebb5937175fbc79e8c0720
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-03-29 04:33:30 +0545

    optimize

--- Web/Day_3/Javascript/index.js
+++ Web/Day_3/Javascript/index.js
@@ -1,27 +1,35 @@
-//Sum of two numbers
-function sum(a,b){
-    return a + b;
+// //Sum of two numbers
+// function sum(a,b){
+//     return a + b;
+// }
+
+// let ans = sum(10,20);
+// console.log(ans);
+
+// //Sum of 1 to n numbers
+
+// function sums(n){
+//     let ans = 0;
+//     for(i = 0; i <= n; i++){
+//         ans = ans + i; // ans += i
+//     }
+//     return ans;
+// }
+
+// const result = sums(100);
+// console.log(result);
+
+// //Optimize Version which run faster too...
+function sum(n){
+    return (n * (n + 1))/2;
 }
 
-let ans = sum(10,20);
-console.log(ans);
-
-//Sum of 1 to n numbers
-
-function sums(n){
-    let ans = 0;
-    for(i = 0; i <= n; i++){
-        ans = ans + i; // ans += i
-    }
-    return ans;
-}
-
-const result = sums(100);
+let result = sum(100);
 console.log(result);
 
-function sumss(a,b){
-    return parseInt(a) + parseInt(b); //Safety purpose
-}
+// function sumss(a,b){
+//     return parseInt(a) + parseInt(b); //Safety purpose
+// }
 
-let results = sumss("10",20);
-console.log(results);
+// let results = sumss("10",20);
+// console.log(results);
