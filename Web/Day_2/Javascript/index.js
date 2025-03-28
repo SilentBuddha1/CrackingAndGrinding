701569646ca7e6d391e52990461a92ccba9cb855
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-03-29 02:18:26 +0545

    assignment

--- Web/Day_2/Javascript/index.js
+++ Web/Day_2/Javascript/index.js
@@ -1,9 +1,38 @@
-let Name = "Ramchandra";
-const age = 23;
-var isStudent = true;
+// let Name = "Ramchandra";
+// const age = 23;
+// var isStudent = true;
 
-console.log(Name);
-console.log(age);
-console.log(isStudent);
+// console.log(Name);
+// console.log(age);
+// console.log(isStudent);
 
 
+function solve(arr) {
+
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].gender === "male" && arr[i].age > 18) {
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
+
+}
+
+const users = [{
+    name: "Ramchadra",
+    age: 23,
+    gender:"male"
+}, {
+    name: "Punam",
+    age: 21,
+    gender: "female"
+}, {
+    name: "Shyam",
+    age: 14,
+    gender: "male"
+}]
+
+let ans = solve(users);
+
+console.log(ans);
