c03348a2b2320f8902dd48d7b47bc975e0212a0c
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-03-29 07:01:15 +0545

    assignment

---
+++ Web/Day_3/Javascript/Assignment/index.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    console.log("Finished...");
+}, 2000);
+
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms)); 
+}
+
+delay(1000).then(() => console.log("2 Second Passes..."));
