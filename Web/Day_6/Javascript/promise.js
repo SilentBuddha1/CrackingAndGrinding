cd7f1145432e8091ebe007e9d6a1dd0f0060586d
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-03-31 22:53:44 +0545

    promise-chaining

--- Web/Day_6/Javascript/promise.js
+++ Web/Day_6/Javascript/promise.js
@@ -4,20 +4,37 @@
 //     });
 // }
 
-
 // setTimeoutPromisified(1000).then(function(callback){
 //     console.log("callback...");
 // })
 
-
 //Callback Hell
 
-setTimeout(function(){
-    console.log("Hi");
-    setTimeout(function(){
-        console.log("Hello");
-        setTimeout(function(){
-            console.log("Print");
-        }, 3000)
-    }, 3000)
-},1000)
+// setTimeout(function(){
+//     console.log("Hi");
+//     setTimeout(function(){
+//         console.log("Hello");
+//         setTimeout(function(){
+//             console.log("Print");
+//         }, 3000)
+//     }, 3000)
+// },1000)
+
+
+//Promise Chaining
+
+function setTimeoutPromisified(duration) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, duration);
+  });
+}
+
+setTimeoutPromisified(1000).then(function () {
+  console.log("Hi");
+  return setTimeoutPromisified(3000).then(function () {
+    console.log("Hello");
+    return setTimeoutPromisified(3000).then(function () {
+      console.log("Print");
+    });
+  });
+});
