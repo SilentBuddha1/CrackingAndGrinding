9a9e41aa49c4f3ca72e0cddba513600ac559e01e
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-03-31 22:43:26 +0545

    CallBackHell

--- Web/Day_6/Javascript/promise.js
+++ Web/Day_6/Javascript/promise.js
@@ -1,10 +1,23 @@
-function setTimeoutPromisified(duration){
-    return new Promise(function(resolve){
-        setTimeout(resolve, duration);
-    });
-}
+// function setTimeoutPromisified(duration){
+//     return new Promise(function(resolve){
+//         setTimeout(resolve, duration);
+//     });
+// }
 
 
-setTimeoutPromisified(1000).then(function(callback){
-    console.log("callback...");
-})
+// setTimeoutPromisified(1000).then(function(callback){
+//     console.log("callback...");
+// })
+
+
+//Callback Hell
+
+setTimeout(function(){
+    console.log("Hi");
+    setTimeout(function(){
+        console.log("Hello");
+        setTimeout(function(){
+            console.log("Print");
+        }, 3000)
+    }, 3000)
+},1000)
