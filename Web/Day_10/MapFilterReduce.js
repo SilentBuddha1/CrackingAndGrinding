893d588289a04ea9ce634eef48c929b455a72e1d
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-04-11 04:23:43 +0545

    map

--- Web/Day_10/MapFilterReduce.js
+++ Web/Day_10/MapFilterReduce.js
@@ -9,8 +9,14 @@
 
 //Introducing Arrow Function
 
-const Square = (a) => a * a;
+// const Square = (a) => a * a;
 
-const Result = Square(20);
+// const Result = Square(20);
+
+// console.log(Result);
+
+const number = [1,2,3,4,5];
+
+const Result = number.map((i) => i * i );
 
 console.log(Result);
