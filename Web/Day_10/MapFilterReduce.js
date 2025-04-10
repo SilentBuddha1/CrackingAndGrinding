f9a1dff50e94257f6e828a581377766adc3d87d9
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-04-11 04:20:06 +0545

    Arrow Function

--- Web/Day_10/MapFilterReduce.js
+++ Web/Day_10/MapFilterReduce.js
@@ -1,8 +1,16 @@
 //Simple Funtion
 
-function square(a){
-    return a * a ;
-}
+// function square(a){
+//     return a * a ;
+// }
 
-const Result = square(10);
-console.log(Result); 
+// const Result = square(10);
+// console.log(Result); 
+
+//Introducing Arrow Function
+
+const Square = (a) => a * a;
+
+const Result = Square(20);
+
+console.log(Result);
