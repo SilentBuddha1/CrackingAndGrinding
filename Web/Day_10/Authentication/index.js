81609b73a777617403706d85ae2684426d52b1bd
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-04-11 20:12:05 +0545

    Athentication

---
+++ Web/Day_10/Authentication/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.post("/signup", function(req, res) {
+
+})
+
+app.post("/signin", function(req,res) {
+
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is running in port 3000....");
+});
