8b5bc7d7f8a4ec1180e171f94172a6a4623122fd
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-04-13 02:21:00 +0545

    Day_12

---
+++ Web/Day_12/BackendAndFrontend/index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const secret = "Learning";
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+function auth(req, res, next) {
+  const token = req.headers.token;
+  const decrypt = jwt.verify(token, secret);
+
+  if (decrypt.username) {
+    req.username = decrypt.username;
+    next();
+  } else {
+    res.json({
+      message: "You are not login",
+    });
+  }
+}
+
+app.post("/signup", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (users.find((u) => u.username == username)) {
+    return res.json({
+      message: "User already exists",
+    });
+  }
+
+  users.push({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "User signup....",
+  });
+  console.log(users);
+});
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = users.find(
+    (u) => u.username == username && u.password == password
+  );
+
+  if (!user) {
+    res.json({
+      message: "User Not Found..",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      username: username,
+    },
+    secret
+  );
+
+  res.json({
+    message: "Successfully Login..",
+    token: token,
+  });
+  console.log(users);
+});
+
+app.get("/me", auth, function (req, res) {
+  const username = req.username;
+  let userFound = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username) {
+      userFound = users[i];
+    }
+  }
+
+  res.json({
+    username: userFound.username,
+    password: userFound.password,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running in port 3000");
+});
