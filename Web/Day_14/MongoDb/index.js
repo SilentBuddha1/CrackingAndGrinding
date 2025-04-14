9fcdcf92bf2db24094f1f9de2266dd7e3f3460eb
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-04-15 02:22:04 +0545

    signup

--- Web/Day_14/MongoDb/index.js
+++ Web/Day_14/MongoDb/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const {UserModel, TodoModel} = require("./db");
+
 
 const app = express();
+app.use(express.json());
 
-app.post("/signup",function(req, res){
+app.post("/signup", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    
+    await UserModel.insert({
+        email: email,
+        password: password,
+        name: name
+    })
 
+    res.json({
+        message: "Signup Succuessfully"
+    })
 })
 
 app.post("/signin", function(req, res){
@@ -15,7 +30,7 @@ app.post("/todo", function(req, res){
 })
 
 app.get("/todos", function(req, res){
-    
+
 })
 
 app.listen(3000, () => {
